8db761c24647286ade72d006082ed7f9f56e3d7e
suyufeng <2533677539@qq.com>
2017-05-31 01:29:12 +0800

    最后一搏

--- src/BE/Ir.java
+++ src/BE/Ir.java
@@ -20,10 +20,12 @@ public class Ir {
     }
     public String tran_reg(Address tmp, Map<Integer, True_address> map) {
         String[] a = new String[13];
-        a[1] = "rcx"; a[2] = "rbx"; a[3] = "r10"; a[4] = "r13";
+/*        a[1] = "rcx"; a[2] = "rbx"; a[3] = "r10"; a[4] = "r13";
         //a[5] = "rbx";a[6] = "r13";a[7] = "r14";a[8] = "r12";
         a[5] = "r11";
-        a[6] = "r14";a[7] = "r8";a[8] = "r12";a[9] = "r9";
+        a[6] = "r14";a[7] = "r8";a[8] = "r12";a[9] = "r9";*/
+        a[1] = "rcx"; a[2] = "r10"; a[3] = "r11"; a[4] = "r9"; a[5] = "r8";
+        a[6] = "rbx";a[7] = "r13";a[8] = "r14";a[9] = "r12";
         a[10] = "rsi"; a[11] = "rdi"; a[12] = "r15";
 
         if(tmp.globel != 0) {
